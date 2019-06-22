f406ef9cb59d99ac0f0bf4bb7d8f4c15f7d1be34
SoheilNamdar <soheilnamdar@yahoo.com>
2019-06-22 15:31:17 +0200

    general 6,7 and 8 cases commit and push

--- src/main/java/model/NumericalRoman.java
+++ src/main/java/model/NumericalRoman.java
@@ -5,9 +5,12 @@ public class NumericalRoman {
 
         String st = "";
 
-        if(number == 6) return "VI";
-        if(number == 7) return "VII";
-        if(number == 8) return "VIII";
+        if(number>=5 && number<=9 ) {
+            st += "V";
+            number -= 5;
+        }
+        /*if(number == 7) return "VII";
+        if(number == 8) return "VIII";*/
 
         for(int i=0 ; i < number ; i++) {
             st += "I";
