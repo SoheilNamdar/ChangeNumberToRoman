4a660139fc0ac5239f5dba8787caf61dfefd46d5
SoheilNamdar <soheilnamdar@yahoo.com>
2019-06-22 15:45:33 +0200

    general cases 11,12 and 13

--- src/main/java/model/NumericalRoman.java
+++ src/main/java/model/NumericalRoman.java
@@ -5,14 +5,17 @@ public class NumericalRoman {
 
         String st = "";
 
-        if(number>=5 && number<=9 ) {
+        if(number >= 5 && number <= 9 ) {
             st += "V";
             number -= 5;
         }
 
-        if(number == 11) return "XI";
-        if(number == 12) return "XII";
-        if(number == 13) return  "XIII";
+        if(number >= 11 && number <= 13) {
+            st += "X";
+            number -= 10;
+        }
+        /*if(number == 12) return "XII";
+        if(number == 13) return  "XIII";*/
 
         for(int i=0 ; i < number ; i++) {
             st += "I";
